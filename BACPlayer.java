d4cb844fbe88269b49e19ef2f70256343a989c06
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2020-03-22 09:55:45 -0400

    Add files via upload

--- BACPlayer.java
+++ BACPlayer.java
@@ -3,19 +3,21 @@ import java.util.*;
 public interface BACPlayer {
 
     /**
-     * Return the author of the player, in form "Lastname, Firstname".
+     * Returns the author of the agent.
+     * @return The author as a string of form "Lastname, Firstname".
      */
     public String getAuthor();
     
     /**
-     * Return the student ID of the the author of this player.
+     * Returns the student ID of  the author of this agent.
+     * @return The author's student ID as a string.
      */
     public String getStudentID();
     
     /**
-     * This method is called exactly once by the BACRunner engine in the beginning. Initialize
-     * whatever internal data structures you use to store the list of possible words to speed
-     * up your future computations.
+     * This method is called exactly once by the {@code BACRunner} engine in the beginning. 
+     * Inside this method, you should initialize whatever internal data structures you use
+     * to store the list of possible words to speed up your future computations.
      */
     public void initializeWordList(List<String> words);
     
@@ -23,8 +25,9 @@ public interface BACPlayer {
      * Given the current state of the game, return the next guess by the player agent.
      * @param n The length of the secret word.
      * @param soFar List of guesses done so far. Empty list, if no guesses have been made.
-     * @param bulls List of number of bulls that each guess in {@code sofar} contained.
-     * @param cows List of number of cows that each guess in {@code sofar} contained.
+     * @param bulls List of number of bulls that each guess in {@code soFar} contained.
+     * @param cows List of number of cows that each guess in {@code soFar} contained.
+     * @return The word chosen by this agent as the next guess.
      */
     public String guess(int n, List<String> guesses, List<Integer> bulls, List<Integer> cows);
     
